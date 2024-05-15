36447d733a2ff6dcd45b85b8ab84870395b9f604
Brijit Adak <brijitadak000@gmail.com>
2024-05-15 20:48:15 +0530

    add

--- src/index.js
+++ src/index.js
@@ -3,3 +3,6 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+//2. Only show the Confirm Password input if userIsRegistered is false.
+//Don't show it if userIsRegistered is true.
